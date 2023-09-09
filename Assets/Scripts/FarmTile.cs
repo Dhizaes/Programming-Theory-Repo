482bf13214543287c48b7b202ad24ebbfe8fb4cb
Dhizaes <dhizaes@protonmail.com>
2023-09-09 19:15:32 +0300

    Add Farmer reference to FarmTile, add method for seed planting

--- Assets/Scripts/FarmTile.cs
+++ Assets/Scripts/FarmTile.cs
@@ -11,8 +11,32 @@ public class FarmTile : MonoBehaviour, IPointerClickHandler
     [SerializeField]
     Sprite FilledSprite;
 
+    private Farmer playerFarmer;
+
+    public bool IsEmpty = true;
+
+    void Start()
+    {
+        playerFarmer = GameObject.Find("Farmer").GetComponent<Farmer>();
+    }
+
     public void OnPointerClick(PointerEventData eventData)
     {
         
+    }
+
+    public void PlantSeed(Base_Crop.ECropTypes selectedCrop)
+    {
+        if (!ReferenceEquals(playerFarmer, null))
+        {
+            if (IsEmpty)
+            {
+
+            }
+        }
+        else
+        {
+            Notification.NotificationManager.SendNotification("Farm tile reference is empty", Notification.ENotificationType.NOTIFICATION_ERROR);
+        }
     }
 }
