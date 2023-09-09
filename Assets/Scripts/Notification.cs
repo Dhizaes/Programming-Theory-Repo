38663c6da5134d04ad0361584242a85dba3f2609
Dhizaes <dhizaes@protonmail.com>
2023-09-09 19:14:54 +0300

    Add notification type as enum, change string based header to notification type enum

--- Assets/Scripts/Notification.cs
+++ Assets/Scripts/Notification.cs
@@ -6,9 +6,20 @@ using UnityEngine.Analytics;
 public class Notification : MonoBehaviour
 {
     public static Notification NotificationManager;
-    public GameObject messageUI;
+
+    [SerializeField]
+    GameObject messageUI;
+    [SerializeField]
     public TMPro.TMP_Text headerText;
+    [SerializeField]
     public TMPro.TMP_Text messageText;
+
+    public enum ENotificationType
+    {
+        NOTIFICATION_NORMAL,
+        NOTIFICATION_WARNING,
+        NOTIFICATION_ERROR
+    }
 
     private void Awake()
     {
@@ -22,13 +33,13 @@ public class Notification : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
-    public void SendNotification(string header, string message)
+    public void SendNotification(string message, ENotificationType notificationType)
     {
         if(!ReferenceEquals(messageUI, null))
         {
             if(!ReferenceEquals(headerText, null) && !ReferenceEquals(messageText, null))
             {
-                Debug.Log(header + ": " + message);
+                Debug.Log(NotificationTypeToString(notificationType) + ": " + message);
             }
         }
     }
@@ -41,6 +52,21 @@ public class Notification : MonoBehaviour
             {
                 Debug.Log(message);
             }
+        }
+    }
+
+    private string NotificationTypeToString(ENotificationType notificationType)
+    {
+        switch(notificationType)
+        {
+            case ENotificationType.NOTIFICATION_NORMAL:
+                return "Notification";
+            case ENotificationType.NOTIFICATION_WARNING:
+                return "Warning";
+            case ENotificationType.NOTIFICATION_ERROR:
+                return "Error";
+            default:
+                return "";
         }
     }
 }
