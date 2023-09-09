af2c24c6e59efc47da0b61fe676cbda29d9f5a8d
Dhizaes <dhizaes@protonmail.com>
2023-09-09 14:32:24 +0300

    Add pointer click event to farm tile

--- Assets/Scripts/FarmTile.cs
+++ Assets/Scripts/FarmTile.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class FarmTile : MonoBehaviour
+public class FarmTile : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField]
     Sprite EmptySprite;
 
     [SerializeField]
     Sprite FilledSprite;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        
+    }
 }
